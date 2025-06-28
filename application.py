38698355527d55ff08860ac8af519d613a8a548d
Divyansh <divyanshsinghnrj@gmail.com>
2025-06-28 21:42:13 +0530

    Deployment setup for aws

--- application.py
+++ application.py
@@ -30,4 +30,4 @@ def predict_datapoint():
         results = pipeline.predict(data_df)
         return render_template('home.html',results=results[0])
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
